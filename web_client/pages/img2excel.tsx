c5973a3663d6eef60e52860ca9914e23bb05e0c2
osawa-koki <github@osawa.dev>
2022-12-30 06:01:03 +0900

    関数をひとつにまとめた。

--- web_client/pages/img2excel.tsx
+++ web_client/pages/img2excel.tsx
@@ -8,7 +8,6 @@ import LinearProgress from '@mui/material/LinearProgress';
 import CircularProgress from '@mui/material/CircularProgress';
 import TextField from '@mui/material/TextField';
 
-import File2Jimp from '../common/File2Jimp';
 import Layout from '../components/Layout';
 import Setting from '../setting';
 
@@ -20,6 +19,31 @@ const Img2ExcelPage = () => {
   let [error, setError] = useState<string | null>(null);
   let [loading, setLoading] = useState(false);
   let [sending, setSending] = useState(false);
+
+  const File2Jimp = async (file: File): Promise<Jimp> => {
+    try {
+      return await new Promise((resolve, reject): void => {
+        const reader = new FileReader();
+        reader.onload = (event: ProgressEvent<FileReader>): void => {
+          if (event.target === null) {
+            reject();
+            return;
+          }
+          const data = event.target.result as ArrayBuffer;
+          Jimp.read(data as Buffer)
+          .then((image: Jimp): void => {
+            resolve(image);
+          })
+          .catch((error: Error): void => {
+            reject(error);
+          });
+        };
+        reader.readAsArrayBuffer(file);
+      });
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
 
   function Draw(jimp: Jimp): void {
     const canvas = document.getElementById('MyCanvas') as HTMLCanvasElement;
