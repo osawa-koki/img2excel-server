3333e36dff9a4867fee5bdc9abf4605515a53ccb
osawa-koki <github@osawa.dev>
2022-12-30 06:31:40 +0900

    タイトル変えた。

--- web_client/setting.ts
+++ web_client/setting.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const Setting = {
   isProd,
-  title: "Img2Excel",
+  title: "Img2Excel 💞💞💞",
   basePath: EnvInfo.basePath,
   apiUri: isProd ? '/api' : 'http://localhost:80/api',
 };
